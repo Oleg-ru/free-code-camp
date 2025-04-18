b65970ac88676ad37607bd81d4de51f66f23fa56
Oleg <oleg_09-95@mail.ru>
2025-04-18 17:50:03 +0300

    Add "Sentence Maker" project

---
+++ js/Sentence Maker.js
@@ -0,0 +1,49 @@
+let adjective = 'Прилагательное';
+let noun = 'Существительное';
+let verb = 'Глагол';
+let place = 'Место';
+let adjective2 = 'Прилагательное_2';
+let noun2 = 'Существительное_2';
+
+let firstStory =
+  'Once upon a time, there was a(n) ' +
+  adjective +
+  ' ' +
+  noun +
+  ' who loved to eat ' +
+  noun2 +
+  '. The ' +
+  noun +
+  ' lived in a ' +
+  place +
+  ' and had ' +
+  adjective2 +
+  ' nostrils that blew fire when it was ' +
+  verb +
+  '.';
+console.log('First story: ' + firstStory);
+
+adjective = 'Прилагательное1';
+noun = 'Существительное1';
+verb = 'Глагол1';
+place = 'Место1';
+adjective2 = 'Прилагательное_21';
+noun2 = 'Существительное_21';
+
+let secondStory =
+  'Once upon a time, there was a(n) ' +
+  adjective +
+  ' ' +
+  noun +
+  ' who loved to eat ' +
+  noun2 +
+  '. The ' +
+  noun +
+  ' lived in a ' +
+  place +
+  ' and had ' +
+  adjective2 +
+  ' nostrils that blew fire when it was ' +
+  verb +
+  '.';
+console.log('Second story: ' + secondStory);
