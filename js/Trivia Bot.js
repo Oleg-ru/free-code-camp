679efe8402472cc3b06b1d48a48eae216ecf61f1
Oleg <oleg_09-95@mail.ru>
2025-04-18 17:26:58 +0300

    Add "Trivia Bot" project

---
+++ js/Trivia Bot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = 'Step-Lep';
+let botLocation = 'RU';
+let favoriteLanguage = 'Java Script';
+console.log('My name is ' + botName + ' and I live on ' + botLocation + '.');
+console.log('My favorite programming language is ' + favoriteLanguage + '.');
+let codingFact = '' + favoriteLanguage + ' is veri interesting language';
+console.log(codingFact);
+codingFact = '' + favoriteLanguage + ' is language programming';
+console.log(codingFact);
+codingFact = 'My ' + favoriteLanguage + ' have many function.';
+console.log(codingFact);
+console.log(
+  'It was fun sharing these facts with you. Goodbye! - ' +
+    botName +
+    ' from ' +
+    botLocation +
+    '.'
+);
