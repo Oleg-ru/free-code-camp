ea623440c21b0d3cddc4eae2c30e9691a6855340
Oleg <oleg_09-95@mail.ru>
2025-04-21 21:12:51 +0300

    Add project "Fortune Teller"

---
+++ js/Fortune Teller.js
@@ -0,0 +1,20 @@
+let fortune1 = 'Your cat will look very cuddly today.';
+let fortune2 = 'The weather will be nice tomorrow.';
+let fortune3 = 'Be cautious of your new neighbours.';
+let fortune4 = 'You will find a new hobby soon.';
+let fortune5 = 'It would be wise to avoid the color red today.';
+
+let randomNumber = Math.ceil(Math.random() * 5);
+let selectedFortune =
+  randomNumber == 1
+    ? fortune1
+    : randomNumber == 2
+    ? fortune2
+    : randomNumber == 3
+    ? fortune3
+    : randomNumber == 4
+    ? fortune4
+    : randomNumber == 5
+    ? fortune5
+    : '';
+console.log(selectedFortune);
