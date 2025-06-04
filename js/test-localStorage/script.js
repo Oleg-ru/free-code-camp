d6e0aec638f1e56f255dd00456e2bdbc3347faaf
Oleg <oleg_09-95@mail.ru>
2025-06-04 20:27:58 +0300

    Add test project on localStorage.

---
+++ js/test-localStorage/script.js
@@ -0,0 +1,47 @@
+class ThemeSwitcher {
+    selector = {
+        switchThemeButton: '[data-js-theme-switcher]'
+    }
+
+    themes = {
+        dark: 'dark',
+        light: 'light',
+    }
+
+    stateClasses = {
+        isDarkTheme: 'is-dark-theme'
+    }
+
+    storageKey = 'theme';
+
+    constructor() {
+        this.switchThemeButtonElement = document.querySelector(this.selector.switchThemeButton);
+        this.setInitialTheme();
+        this.bindEvents();
+    }
+
+    get isDarkThemeCached() {
+        return localStorage.getItem(this.storageKey) === this.themes.dark;
+    }
+
+    setInitialTheme() {
+        this.isDarkThemeCached;
+        document.documentElement.classList.toggle(this.stateClasses.isDarkTheme, this.isDarkThemeCached)
+    };
+
+    onClick = ()=> {
+        localStorage.setItem(
+            this.storageKey,
+            this.isDarkThemeCached ? this.themes.light : this.themes.dark
+        )
+        document.documentElement.classList.toggle(this.stateClasses.isDarkTheme);
+    }
+
+    bindEvents() {
+        this.switchThemeButtonElement.addEventListener('click', this.onClick)
+    };
+
+
+}
+
+new ThemeSwitcher();
