895d2bbb6faeee393274b4b83e996ef8dbdb0efc
Oleg <oleg_09-95@mail.ru>
2025-10-27 21:24:50 +0300

    Add lab "Sorted Index Finder"

---
+++ js/Sorted Index Finder.js
@@ -0,0 +1,8 @@
+function getIndexToIns(array, number) {
+    array.sort((a, b) => a - b);
+    return array.findIndex(el => el >= number)
+}
+
+// console.log(getIndexToIns([1, 2, 3, 4], 1.5) )
+// console.log(getIndexToIns([20, 3, 5], 19))
+// console.log(getIndexToIns([3, 10, 5], 3))
